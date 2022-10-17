f6ff64da23900813893ba384e510c27ba0095958
mohRamy <mohammedrdaly@gmail.com>
2022-10-18 07:00:26 +1200

    df

--- lib/features/home/services/home_services.dart
+++ lib/features/home/services/home_services.dart
@@ -10,14 +10,11 @@ import '../../../common/utils/constants/error_handling.dart';
 import '../../../common/utils/constants/global_variables.dart';
 import '../../../provider/user_provider.dart';
 
-class HomeServices{
-
-  Future<List<ProductModel>> fetchCategoryProduct(
-    {
-      required BuildContext context,
-      required String category,
-    }
-  )async{
+class HomeServices {
+  Future<List<ProductModel>> fetchCategoryProduct({
+    required BuildContext context,
+    required String category,
+  }) async {
     final userProvider = Provider.of<UserProvider>(context, listen: false);
     List<ProductModel> productList = [];
     try {
@@ -47,5 +44,38 @@ class HomeServices{
       Components.showSnackBar(context, e.toString());
     }
     return productList;
+  }
+
+  Future<ProductModel> fetchDealOfDay({
+    required BuildContext context,
+  }) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    ProductModel product = ProductModel(
+      name: '',
+      description: '',
+      quantity: 0,
+      images: [],
+      category: '',
+      price: 0,
+    );
+
+    try {
+      http.Response res =
+          await http.get(Uri.parse('$uri/api/deal-of-day'), headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'x-auth-token': userProvider.user.token,
+      });
+
+      httpErrorHandle(
+        res: res,
+        context: context,
+        onSuccess: () {
+          product = ProductModel.fromJson(res.body);
+        }, 
+      );
+    } catch (e) {
+      Components.showSnackBar(context, e.toString());
+    }
+    return product;
   }
 }
